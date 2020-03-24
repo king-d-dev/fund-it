b844329c5f44f9f1ce48ed20579b6cc9e2e24aea
King-alpha <davidoreal1@gmail.com>
2020-03-24 14:41:05 -0700

    made changes to properly handle profile pictuer upload

--- passport-config.js
+++ passport-config.js
@@ -1,12 +1,12 @@
-const passport = require("passport");
-const { Strategy, ExtractJwt } = require("passport-jwt");
-const mongoose = require("mongoose");
-const User = mongoose.model("User");
+const passport = require('passport');
+const { Strategy, ExtractJwt } = require('passport-jwt');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
 const keys = process.env.JWT_SECRET;
 
 const jwtOptions = {
   secretOrKey: keys,
-  jwtFromRequest: ExtractJwt.fromHeader("authenticate")
+  jwtFromRequest: ExtractJwt.fromHeader('authenticate')
 };
 
 const jwtStrategy = new Strategy(jwtOptions, async function(payload, done) {
@@ -17,6 +17,8 @@ const jwtStrategy = new Strategy(jwtOptions, async function(payload, done) {
     if (!user) {
       return done(null, false);
     }
+    // make sure user's password is not mistakenly sent to the client
+    delete user.password;
     return done(null, user);
   } catch (error) {
     return done(error, false);
