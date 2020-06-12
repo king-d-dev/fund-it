7153410985c343b67c8770f07b11fb47a5af4655
King-alpha <davidoreal1@gmail.com>
2020-06-12 00:58:58 -0700

    renamed index.html file in public folder

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const http = require("http");
-const path = require("path");
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const fileUpload = require("express-fileupload");
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
 
-if (process.env.NODE_ENV !== "PRODUCTION") require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true
+  useCreateIndex: true,
 });
 
 mongoose.connection
-  .once("open", () => console.log("SUCCESS: Database Connection"))
-  .on("error", () => console.log("ERROR: Database Connection"));
+  .once('open', () => console.log('SUCCESS: Database Connection'))
+  .on('error', () => console.log('ERROR: Database Connection'));
 
 const app = express();
 
@@ -24,17 +24,17 @@ app.use(fileUpload());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-require("./models");
-require("./passport-config");
-require("./routes")(app);
+require('./models');
+require('./passport-config');
+require('./routes')(app);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname, "client", "build")));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
@@ -42,4 +42,4 @@ const PORT = process.env.PORT || 3005;
 http
   .createServer(app)
   .listen(PORT)
-  .on("listening", () => console.log(`Server running on PORT ${PORT}`));
+  .on('listening', () => console.log(`Server running on PORT ${PORT}`));
